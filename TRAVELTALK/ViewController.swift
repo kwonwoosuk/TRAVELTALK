a0917d08a7970abb0a0838e775b8cbe91b49cc62
kwonws <kevincool6@naver.com>
2025-01-11 21:36:47 +0900

    Initial Commit

---
+++ TRAVELTALK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TRAVELTALK
+//
+//  Created by 권우석 on 1/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
